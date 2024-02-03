b4c4983e7fa5a03e90805b1b24d0a5a6375b23c4
inoati1991@gmail.com <inoati1991@gmail.com>
2024-02-03 18:47:04 +0100

    Updated context path to budget ducklings inc for better url presentation

--- src/main/java/servlets/SignupServlet.java
+++ src/main/java/servlets/SignupServlet.java
@@ -4,11 +4,12 @@ package servlets;
 import  javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SignupServlet extends HttpServlet {
-    public void service(HttpServletRequest request, HttpServletResponse response){
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
 
-        System.out.println("this is the username" + username);
+        response.getWriter().println("this is the username " + username);
     }
 }
--- src/main/webapp/signup.jsp
+++ src/main/webapp/signup.jsp
@@ -13,7 +13,7 @@
             </div>
             <div class="text-center mt-3">
                 <!-- Add your signup form here -->
-                <form action="/0/signup" method="post">
+                <form action="/budget-ducklings-inc/signup" method="post">
                     <label for="username">Username:</label>
                     <input type="text" id="username" name="username" required>
                     <br>
