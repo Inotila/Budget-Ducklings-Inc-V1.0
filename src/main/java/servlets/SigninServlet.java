c7e9826dbc9ea8adc62026559d827f7d4b36df41
inoati1991@gmail.com <inoati1991@gmail.com>
2024-02-03 21:45:51 +0100

    Added annotation to servlets and removed xml for easier future maintaince

--- src/main/java/servlets/SigninServlet.java
+++ src/main/java/servlets/SigninServlet.java
@@ -1,11 +1,12 @@
 package servlets;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
+@WebServlet("/signin")
 public class SigninServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession();
--- src/main/java/servlets/SignupServlet.java
+++ src/main/java/servlets/SignupServlet.java
@@ -1,12 +1,13 @@
 
 package servlets;
 
+import javax.servlet.annotation.WebServlet;
 import  javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-
+@WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
