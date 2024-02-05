bcb2a01eb91e2492d5cd056e55232a6c4632998e
inoati1991@gmail.com <inoati1991@gmail.com>
2024-02-05 12:39:14 +0100

    Added set session attribute to be used later to secure pages

--- src/main/java/dao/ProfileDao.java
+++ src/main/java/dao/ProfileDao.java
@@ -1,7 +1,6 @@
 package dao;
 
 import connectors.DbConnector;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
@@ -19,6 +20,8 @@ public class LoginServlet extends HttpServlet {
         boolean isAuthenticated = profileDao.authenticateUser(username, password);
 
         if (isAuthenticated) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
             // Redirect to the home page
             response.sendRedirect("/index.jsp");
         } else {
