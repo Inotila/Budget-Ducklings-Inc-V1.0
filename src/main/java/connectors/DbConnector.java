6309c8cc52320adbc07b550be42a9e1b3c73717a
inoati1991@gmail.com <inoati1991@gmail.com>
2024-02-08 21:48:19 +0100

    added dbconnector

---
+++ src/main/java/connectors/DbConnector.java
@@ -0,0 +1,22 @@
+package connectors;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnector {
+    public static Connection getConnection() throws SQLException {
+        try {
+            // load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        }
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/budget_ducklings_inc";
+        String username = "root";
+        String password = "Chicken1sDaB3st@1010";
+        Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        return connection;
+    }
+}
